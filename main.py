37f8d42457159da7d1b58a26f2b68e048929f193
mola1129 <ms.mola1129@gmail.com>
2020-01-21 04:48:21 +0900

    Add cloudinary.

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import cloudinary
+import cloudinary.uploader
+import requests
 import scraping.scraping as sc
 import urldata.setup as url
 import line.notify as line
 import os
 
-print(os.environ["HOME"])
+# 記事のURLを取得
+news_urls = sc.get_news_urls_from_oishi()
 
-urls = sc.get_news_urls_from_oishi()
-diff = url.get_diff_urls(urls)
-url.save_urls('urls.txt', urls)
+cloud_name = os.environ['CLOUD_NAME']
+api_key = os.environ['API_KEY']
+api_secret = os.environ['API_SECRET']
+cloudinary.config(
+    cloud_name=cloud_name,
+    api_key=api_key,
+    api_secret=api_secret
+)
+# 前回の記事URLを取得
+res = cloudinary.api.resources(
+    type="upload", resource_type="raw", prefix="oishi-news")
+file_url = res["resources"][0]["secure_url"]
+r = requests.get(file_url)
+with open('urls.txt', mode='w') as f:
+    f.write(r.text)
+
+# 記事の差分を抽出
+diff = url.get_diff_urls(news_urls, 'urls.txt')
+url.save_urls('urls.txt', news_urls)
+
+# Line Notifyで通知
 line_token = os.environ['LINE_TOKEN']
 if diff != []:
     message = '\n'
--- urldata/setup.py
+++ urldata/setup.py
@@ -1,9 +1,10 @@
 import numpy as np
-old_urls = np.loadtxt('urls.txt', dtype='unicode')
+import cloudinary.uploader
 
 
-def get_diff_urls(new_urls):
+def get_diff_urls(new_urls, filename):
     new_urls = np.array(new_urls)
+    old_urls = np.loadtxt(filename, dtype='unicode')
     diff = []
     for new in new_urls:
         if not (new in old_urls):
@@ -14,3 +15,5 @@ def get_diff_urls(new_urls):
 def save_urls(filename, urls):
     urls = np.array(urls)
     np.savetxt(filename, urls, fmt="%s")
+    cloudinary.uploader.upload(
+        file=filename, folder="oishi-news/", use_filename=True, unique_filename=False, resource_type="raw")
